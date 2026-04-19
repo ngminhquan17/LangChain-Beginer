5ebb0849e9e653c533ac1d84fb86057582a66292
ngminhquan17 <nguyenminhquan17022003@gmail.com>
2026-04-19 20:26:34 +0900

    Building Chatbot Using Paid And Open Source LLM's using Langchain And Ollama

---
+++ chatbot/app.py
@@ -0,0 +1,38 @@
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+import streamlit as st
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+os.environ["OPENAI_API_KEY"]=os.getenv("OPENAI_API_KEY")
+# langsmith tracking
+os.environ["LANGCHAIN_TRACING_V2"]="true"
+os.environ["LANGCHAIN_API_KEY"]=os.getenv("LANGCHAIN_API_KEY")
+
+# prompt template
+
+promt=ChatPromptTemplate.from_messages(
+    [
+        ("system", "You are a helpful assistant. Please response to the user question"),
+        ("user", "Question:{question}")
+    ]
+)
+
+# streamlit framework
+
+st.title('Langchain Demo with OPENAI API')
+input_text=st.text_input("Search the topic you want")
+
+# openAI llm
+llm=ChatOpenAI(model="gpt-3.5-turbo")
+output_parser=StrOutputParser()
+chain=promt|llm|output_parser
+
+if input_text:
+    st.write(chain.invoke({'question':input_text}))
+
+print("OKE")
